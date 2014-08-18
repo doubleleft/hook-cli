d5f7775922177c6cba82c99d002461d49d103812
Endel Dreyer <endel.dreyer@gmail.com>
2014-08-18 15:25:54 -0300

    generators coding style

--- templates/route.php
+++ templates/route.php
@@ -2,9 +2,10 @@
 /**
  * {method_uppercase} {path}
  */
-$app->{method}('{path}', function({arguments}) use ($app) {
-	//
-	// do something
-	//
-	$app->content = array('success' => true);
+$app->{method}('{path}', function({arguments}) use ($app)
+{
+    //
+    // do something
+    //
+    $app->content = array('success' => true);
 });
